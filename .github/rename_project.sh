188ba870e617b12354deb9bbbab1a34c3cd78a40
Robert Coltheart <robert.coltheart@gmail.com>
2023-07-10 20:08:49 +1000

    Initial commit

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -1,36 +1,33 @@
 #!/usr/bin/env bash
-while getopts a:n:u:d: flag
+
+while getopts a:n:u: flag
 do
     case "${flag}" in
         a) author=${OPTARG};;
         n) name=${OPTARG};;
-        u) urlname=${OPTARG};;
-        d) description=${OPTARG};;
+        u) url=${OPTARG};;
     esac
 done
 
 echo "Author: $author";
-echo "Project Name: $name";
-echo "Project URL name: $urlname";
-echo "Description: $description";
+echo "Project name: $name";
+echo "Project url: $url";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
-# for filename in $(find . -name "*.*") 
-for filename in $(git ls-files) 
+original_author="(author)"
+original_name="Prefab"
+original_url="(repo)"
+
+for filename in $(git ls-files)
 do
     sed -i "s/$original_author/$author/g" $filename
     sed -i "s/$original_name/$name/g" $filename
-    sed -i "s/$original_urlname/$urlname/g" $filename
-    sed -i "s/$original_description/$description/g" $filename
-    echo "Renamed $filename"
+    sed -i "s/$original_url/$url/g" $filename
 done
 
-mv project_name $name
+find . -type d -exec rename "s/Prefab/${name}/g" {} +
+find . -type f -exec rename "s/Prefab/${name}/g" {} +
 
-# This command runs only once on GHA!
 rm -rf .github/template.yml
+rm -rf .github/workflows/rename-project.yml
